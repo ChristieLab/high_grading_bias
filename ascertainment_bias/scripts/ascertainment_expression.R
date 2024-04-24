19c19c39271c7f07641607e2d20d7c0941ab39de
Andy Lee <andymuanlee@gmail.com>
2024-04-24 11:50:37 -0400

    re-configure directories

---
+++ ascertainment_bias/scripts/ascertainment_expression.R
@@ -0,0 +1,76 @@
+
+### Ascertainment Bias in expression data 
+### Randomly shuffle treatments in TAE to test the effects
+
+
+gene.counts <- read.table("tae_genome_featureCount_counts.txt", sep="\t", header=T, row.names=1, check.names=F)
+drop.vars   <- names(gene.counts) %in% c("Chr","Start", "End", "Strand", "Length")
+gene.counts <- gene.counts[!drop.vars]
+head(gene.counts)
+
+samples <- read.csv("../tae_sample_info.csv")
+rownames(samples) <- samples$sample_bam
+all(rownames(samples) == colnames(gene.counts)) # should be TRUE 
+
+samples$true_treat <- samples$treatment
+samples$treatment <- sample(LETTERS[1:2], nrow(samples), TRUE)
+
+
+### examine response to temperature in each site -----------------------------##
+#----Sample processing ----#
+
+site <- which(samples$site == "NAP") 
+site.meta   <- samples[site, ] 
+site.genecounts <- gene.counts[, site]
+
+all(rownames(site.meta) %in% colnames(site.genecounts)) # check sample names are in the same order in the gene count sample info table
+all(rownames(site.meta) == colnames(site.genecounts)) # must  be TRUE or do not proceed
+
+dds.site <- DESeqDataSetFromMatrix(countData = site.genecounts,
+                                   colData = site.meta,
+                                   design = ~treatment) 
+dds.site$treatment <- relevel(dds.site$treatment, "A")
+dds.site
+
+## gene names 
+# renames genes in a DESeqDataSet to match gene names in the reference annotation (i.e., replaces MSTRG wherever gene name is known)
+assembly <- readGFF("tae_all_merged_genome.gtf")             ## read in merged GTF
+gene_idx <- match(dds.site@rowRanges@partitioning@NAMES, assembly$gene_id)
+
+# create gene_names, which is a table linking gene name, transcript ID, and xloc information for later distinguishing different isoforms of the same gene (same gene start and end) and transcripts generated from different paralogs of the same gene (by xloc)
+contig.name      <- as.character(assembly$seq[gene_idx])
+transcript_start <- assembly$start[gene_idx]
+transcript_end   <- assembly$end[gene_idx]
+transcript.id  <- assembly$transcript_id[gene_idx]
+gene_names     <- cbind(contig.name, transcript_start, transcript_end, transcript.id)
+
+dds.site@rowRanges@partitioning@NAMES <- paste(gene_names[,1],gene_names[,2],gene_names[,3],gene_names[,4], sep=",") # adds unique gene names to dds
+which(duplicated(dds.site@rowRanges@partitioning@NAMES)) # check to make sure that no gene names in dds are duplicates
+
+## Run DESeq 
+dds.site <- DESeq(dds.site)
+res.site <- results(dds.site, alpha=0.05, pAdjustMethod = "BH", independentFiltering = TRUE)
+length(which(res.site[, 6] < 0.05))
+summary(res.site)
+
+
+## TEST CODE
+top_lfc <- which(abs(res.site$log2FoldChange) > quantile(abs(res.site$log2FoldChange), .95, na.rm = T)) 
+
+top_lfc <- which(abs(res.site$log2FoldChange) > quantile(abs(res.site$log2FoldChange), .99999, na.rm = T)) 
+
+top_lfc_vst <- varianceStabilizingTransformation(dds.site[top_lfc, ])
+
+
+plot.all.data <- plotPCA(top_lfc_vst, intgroup = "treatment", returnData=TRUE)
+percentVar <- round(100*attr(plot.all.data, "percentVar"))
+
+## color PCA by treatment
+ggplot(plot.all.data, aes(PC1, PC2, color=treatment, shape=treatment)) +
+  # ggtitle(paste("Top",num.genes,"DEGs, padj < ",p.cutoff,"and log2FC >",fc.cutoff)) +
+  scale_color_manual(values=c("darkblue","firebrick")) + 
+  geom_point(size=3) +
+  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+  coord_fixed()
+
---
+++ ascertainment_bias/scripts/rubias_loo.R
@@ -0,0 +1,50 @@
+### RUBIAS loo self assignment ====
+library(rubias); library(tidyverse); library(snpR)
+        
+# setwd("~/assignment_power_analyses/data")
+
+args <- commandArgs(TRUE) 
+data <- readRDS(as.character(args[1]))
+outfile <- as.character(args[2])
+
+### format input data 
+
+### Create functions to sort data and run the sa function in rubias 
+### the data frame must have these 4 columns: sample_type, repunit, collection, indiv
+rubias_data_setup <- function(data){
+  sample.meta(data)$indiv              <- sample.meta(data)[,1]
+  # collection populations
+  data_rafm <- format_snps(data, facets="indiv", "rafm") 
+  data_rafm$collection         <- sample.meta(data)[,2]
+  data_rafm$sample_type        <- "reference"
+  data_rafm$repunit            <- "NA" # NA as we are just using the
+  data_rafm <- data_rafm %>%
+    select(indiv, sample_type, repunit, collection, everything())
+  return(data_rafm)
+  }
+run_rubias_sa <- function(data_rafm){
+  sa <- self_assign(reference = data_rafm, gen_start_col = 5)
+  sa_to_site <- sa %>%
+    group_by(indiv, collection, repunit, inferred_collection) %>%
+    summarise(repu_scaled_like = sum(scaled_likelihood)) %>% 
+    group_by(indiv) %>%
+    filter(repu_scaled_like==max(repu_scaled_like)) 
+  return(sa_to_site)
+  # add line to write result text files and include dataset name
+}
+
+### Run the codes 
+rubias_input <- rubias_data_setup(data)
+sa_result    <- run_rubias_sa(rubias_input)
+
+##   sa_to_site   <- sa_result %>% 
+##     #group_by(indiv, collection, repunit, inferred_collection) %>%
+##     #summarise(repu_scaled_like = sum(scaled_likelihood)) %>% 
+##     group_by(indiv) %>%
+##     filter(repu_scaled_like==max(repu_scaled_like)) %>%
+##     summarise(percentage_match = sum(collection == inferred_collection) / n()) 
+##   mean(sa_to_site$percentage_match)
+
+#=========save=======================
+outfile <- paste0(outfile, ".txt")
+fwrite(sa_result, outfile, col.names = TRUE, row.names = FALSE, sep = "\t")
