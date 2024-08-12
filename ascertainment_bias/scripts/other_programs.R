f4df8710294d3fa56c214d4e31afe5dfeb8896d2
hemstrow <hemstrow@gmail.com>
2024-08-12 13:25:06 -0700

    SI and script updates

--- ascertainment_bias/scripts/other_programs.R
+++ ascertainment_bias/scripts/other_programs.R
@@ -6,7 +6,7 @@ library(khroma)
 library(cowplot)
 library(ggplot2)
 
-panmictic <- readRDS("../ascertainment_bias/data/ms_out_1.RDS")  
+panmictic <- readRDS("../data/ms_out_1.RDS")  
 panmictic <- calc_global_fst(panmictic, "pop")
 fst <- get.snpR.stats(panmictic, "pop", "fst")
 high_fst <- which(fst$pairwise$fst >= quantile(fst$pairwise$fst, .95, na.rm = TRUE))
@@ -25,23 +25,23 @@ colours <- khroma::color("batlow")
 manual_colors <- colours(4, range=c(0.1,0.8))
 
 ### all SNPs 
-p1 <- plot_clusters(panmictic_sub, "pop", alt.palette = manual_colors, plot_type = "tSNE")
-p2 <- plot_clusters(panmictic_sub, "pop", alt.palette = manual_colors, plot_type = "umap")
-p3 <- plot_clusters(panmictic_sub, "pop", alt.palette = manual_colors, plot_type = "dapc")
-p4 <- plot_structure(panmictic_sub, "pop", alt.palette = manual_colors, method = "snmf", k = 2:4)
+p1 <- plot_clusters(panmictic_sub, "pop", alt.palette = manual_colors, plot_type = c("tSNE", "umap"))
+p3 <- plot_clusters(panmictic_sub, "pop", alt.palette = manual_colors, plot_type = "dapc", dapc_clustering_max_n_clust = NULL,
+                    dapc_clustering_npca = 100, dapc_clustering_nclust = 4, dapc_npca = 100, dapc_ndisc = 3) #100, 4, 100, 3
+p4 <- plot_structure(panmictic_sub, "pop", alt.palette = manual_colors, method = "snmf", k = 4)
 
 ### high fst 
-p6 <- plot_clusters(panmictic.top, "pop", alt.palette = manual_colors,  plot_type = "tSNE")
-p7 <- plot_clusters(panmictic.top, "pop", alt.palette = manual_colors,  plot_type = "umap")
-p8 <- plot_clusters(panmictic.top, "pop", alt.palette = manual_colors,  plot_type = "dapc") #300, 4, 300, 3, 
-p9 <- plot_structure(panmictic.top, "pop", alt.palette = manual_colors, method = "snmf", k = 2:4)
+p6 <- plot_clusters(panmictic.top, "pop", alt.palette = manual_colors,  plot_type = c("tSNE", "umap"))
+p8 <- plot_clusters(panmictic.top, "pop", alt.palette = manual_colors,  plot_type = "dapc", dapc_clustering_max_n_clust = NULL, 
+                    dapc_clustering_npca = 150, dapc_clustering_nclust = 4, dapc_npca = 150, dapc_ndisc = 3) # 50% of var explained
+p9 <- plot_structure(panmictic.top, "pop", alt.palette = manual_colors, method = "snmf", k = 4)
 
 
 
 fig <- plot_grid(p1$plots$tsne + theme(legend.position="none"), 
           p6$plots$tsne + theme(legend.position="none"), 
-          p2$plots$umap + theme(legend.position="none"), 
-          p7$plots$umap + theme(legend.position="none"), 
+          p1$plots$umap + theme(legend.position="none"), 
+          p6$plots$umap + theme(legend.position="none"), 
           p3$plots$dapc + theme(legend.position="none"), 
           p8$plots$dapc + theme(legend.position="none"), 
           p4$plot + theme(legend.position="none"),       
@@ -54,4 +54,4 @@ fig <- plot_grid(p1$plots$tsne + theme(legend.position="none"),
           hjust = 0,
           axis="tbl")  
 
-save_plot("~/downloads/si.pdf", fig,  base_height = 15, base_width = 11)
+save_plot("../results/Figure_S1.pdf", fig,  base_height = 15, base_width = 11)
