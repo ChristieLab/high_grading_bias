16dd929b8fe91cab91b3daf8cd1fe25f8b365a0a
Andy Lee <andymuanlee@gmail.com>
2024-10-14 14:27:12 -0400

    Update fig 1 with bigger fonts

--- ascertainment_bias/scripts/Figure1.R
+++ ascertainment_bias/scripts/Figure1.R
@@ -41,76 +41,76 @@ rubias_high <- read.table("~/assignment_tests/ascertainment_bias/results/rubias/
 
 p_rubias_all <- ggplot(rubias_all) +
   geom_bar(aes(fill=inferred_collection, x=collection), position="fill", width =0.975) + 
-  labs(x= "Randomly Assigned Population",  y="Proportion of Individuals Assigned") +
+  labs(x= "Randomly Assigned Population",  y="Prop. of Individuals Assigned") +
   scale_fill_manual(values=monarch_colors[3:4]) +
   theme_bw() + 
   guides(fill=guide_legend(title = stringr::str_wrap("Inferred Population", width =20)))
   
 p_rubias_high <- ggplot(rubias_high) +
   geom_bar(aes(fill=inferred_collection, x=collection), position="fill", width =0.975) + 
-  labs(x= "Randomly Assigned Population",  y="Proportion of Individuals Assigned") +
+  labs(x= "Randomly Assigned Population",  y="Prop. of Individuals Assigned") +
   scale_fill_manual(values=monarch_colors)+ theme_bw() + 
   guides(fill=guide_legend(title = stringr::str_wrap("Inferred Population", width =20)))
 
 library(cowplot)
 library(ggpubr)
 top_lg <- get_legend(pca_all$plots$pca + 
-                       theme(legend.title = element_text(size=14),
-                             legend.text = element_text(size=12)))
+                       theme(legend.title = element_text(size=20),
+                             legend.text = element_text(size=16)))
 mid_lg <- get_legend(structure_allsnps$plot + 
-                       theme(legend.title = element_text(size=14),
-                             legend.text = element_text(size=12)))
+                       theme(legend.title = element_text(size=20),
+                             legend.text = element_text(size=16)))
 btm_lg <- get_legend(p_rubias_high + 
-                       theme(legend.title = element_text(size=14),
-                             legend.text = element_text(size=12)))
+                       theme(legend.title = element_text(size=20),
+                             legend.text = element_text(size=16)))
 
 figure_1 <- plot_grid(
   pca_all$plots$pca + 
     guides(color="none") + 
     ggtitle("All SNPs") + 
     theme(plot.title = element_text(size=20), 
-          axis.text = element_text(angle=0, size=12), 
-          axis.title = element_text(size=14)), 
+          axis.text = element_text(angle=0, size=18), 
+          axis.title = element_text(size=18)), 
   
   pca_high$plots$pca + guides(color="none") + 
     ggtitle(expression('Top' * ' ' * '5%' * ' ' *italic("F")[ST])) + 
     theme(plot.title = element_text(size=20), 
-          axis.text = element_text(angle=0, size=12), 
-          axis.title = element_text(size=14)), 
+          axis.text = element_text(angle=0, size=18), 
+          axis.title = element_text(size=18)), 
   top_lg,
   
   structure_allsnps$plot + 
     guides(fill="none", color="none") + 
     xlab("Randomly Assigned Population") + 
-    theme(axis.text = element_text(angle=0, size=12), 
+    theme(axis.text = element_text(angle=0, size=18), 
           axis.text.x = element_text(angle=0),
-          axis.title = element_text(size=14)),
+          axis.title = element_text(size=18)),
   
   structure_highfst$plot + 
     guides(fill="none", color="none") + 
     xlab("Randomly Assigned Population") + 
-    theme(axis.text = element_text(size=12), 
+    theme(axis.text = element_text(size=18), 
           axis.text.x = element_text(angle=0),
-          axis.title = element_text(size=14)),
+          axis.title = element_text(size=18)),
   mid_lg,
   
   p_rubias_all + guides(fill="none") + 
     scale_x_discrete(expand = c(0,0)) + 
     scale_y_continuous(expand = c(0,0)) + 
-    theme(axis.text = element_text(angle=0, size=12), 
-          axis.title = element_text(size=14)),
+    theme(axis.text = element_text(angle=0, size=18), 
+          axis.title = element_text(size=18)),
   
   p_rubias_high + guides(fill="none") + 
     scale_x_discrete(expand = c(0,0)) + 
     scale_y_continuous(expand = c(0,0)) +
-    theme(axis.text = element_text(angle=0, size=12),
-          axis.title = element_text(size=14)), 
+    theme(axis.text = element_text(angle=0, size=18),
+          axis.title = element_text(size=18)), 
   
   btm_lg,
-  ncol = 3, axis = "tr", align="hv", rel_widths =c(1,1,.3)
+  ncol = 3, axis = "tr", align="hv", rel_widths =c(1,1,.42)
 )
 figure_1
-save_plot("~/assignment_tests/ascertainment_bias/results/Figure1.pdf", figure_1,  base_height = 11, base_width = 15)
+save_plot("~/assignment_tests/ascertainment_bias/figures/fig1.svg", figure_1,  base_height = 11, base_width = 15)
 
 ## plot other methods for SI 
 plot_clusters(monarchs_high, "pop", "tsne")
